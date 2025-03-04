696ed439a9e03e5665bcddec4d1e61b2a97fedee
ridgit <ridouwanyuna@gmail.com>
2025-03-04 06:20:32 -0500

    Add files via upload

---
+++ functions/verbeccsvc/verbeccsvc/__init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+
+__version__ = '1.5.9'
+
+app = FastAPI(title='verbecc-svc')
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+import verbecc_svc.views
